8394aa2c7834d0ad4f2dfae6cf152ba6480957d7
rebxxt <allrebxxt@gmail.com>
2021-09-24 03:57:10 +0300

    Last update

--- api/repository/transport.py
+++ api/repository/transport.py
@@ -7,12 +7,19 @@ def transport_multiset(transports: UserTransportMultisetSerializer):
     new_transports = []
 
     for transport in transports.data['transports']:
-        (new_transport, _) = UserTransportModel.objects.get_or_create(
-            pk=transport['id'],
-            user_id=transports.data['user'],
-            registration_number=transport['registration_number'],
-            type_id=transport['type'],
-            info=transport['info'])
+        try:
+            new_transport = UserTransportModel.objects.get(pk=transport['id'])
+            new_transport.user_id = transports.data['user']
+            new_transport.registration_number = transport['registration_number']
+            new_transport.type_id = transport['type']
+            new_transport.info = transport['info']
+            new_transport.save()
+        except BaseException:
+            new_transport = UserTransportModel.objects.create(
+                user_id=transports.data['user'],
+                registration_number=transport['registration_number'],
+                type_id=transport['type'],
+                info=transport['info'])
         new_transports = [*new_transports, new_transport]
 
     transport_numbers = list(map(lambda x: x.registration_number, new_transports))
